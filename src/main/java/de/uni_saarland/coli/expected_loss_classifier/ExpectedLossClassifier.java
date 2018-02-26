18d661ba2f459089f04842186426b68f0cce641d
Christoph Teichmann <cteichmann@coli.uni-saarland.de>
2018-02-26 18:53:39 +0100

    Wrote gradient computation for softmax in ELC.
    Next step is learning.

--- src/main/java/de/uni_saarland/coli/expected_loss_classifier/ExpectedLossClassifier.java
+++ src/main/java/de/uni_saarland/coli/expected_loss_classifier/ExpectedLossClassifier.java
@@ -19,8 +19,8 @@ public class ExpectedLossClassifier {
     public ExpectedLossClassifier(int inputDimensions, int[] resultDimensions) {
         this.weights = new double[resultDimensions.length][][];
 
-        for(int i=0;i<weights.length;++i) {
-            int inDim = i==0 ? inputDimensions : resultDimensions[i-1];
+        for (int i = 0; i < weights.length; ++i) {
+            int inDim = i == 0 ? inputDimensions : resultDimensions[i - 1];
             int outDim = resultDimensions[i];
 
             double[][] layerWeights = new double[outDim][inDim];
@@ -85,7 +85,7 @@ public class ExpectedLossClassifier {
     public double[][][] gradient(double[] inputs, double[] losses) {
         double[][] outputs = this.compute(inputs);
 
-        return this.gradient(inputs,losses,outputs);
+        return this.gradient(inputs, losses, outputs);
     }
 
     /**
@@ -99,35 +99,43 @@ public class ExpectedLossClassifier {
         double[][][] parameterGrads = new double[outputs.length][][];
         double[][] nonLinGrads = new double[outputs.length][];
 
-        for(int layer=outputs.length-1;layer>=0;--layer) {
-                double[] multipliers = (layer+1 == outputs.length) ? losses : nonLinGrads[layer+1];
-                double[] ins = (layer==0) ? inputs : outputs[layer-1];
+        for (int layer = outputs.length - 1; layer >= 0; --layer) {
+            double[] multipliers = (layer + 1 == outputs.length) ? losses : nonLinGrads[layer + 1];
+            double[] ins = (layer == 0) ? inputs : outputs[layer - 1];
 
-                double[][] paraGrads = new double[outputs[layer].length][];
-                parameterGrads[layer] = paraGrads;
+            double[][] paraGrads = new double[outputs[layer].length][];
+            parameterGrads[layer] = paraGrads;
 
-                double[] nonLGrads = new double[ins.length];
-                nonLinGrads[layer] = nonLGrads;
+            double[] nonLGrads = new double[ins.length];
+            nonLinGrads[layer] = nonLGrads;
 
+            for (int cell = 0; cell < paraGrads.length; ++cell) {
+                double[] local = new double[weights[layer][cell].length];
+                if (layer + 1 != outputs.length && multipliers[cell] <= 0) {
+                    Arrays.fill(local, 0.0);
+                    continue;
+                }
 
-                for(int cell=0;cell < paraGrads.length;++cell) {
-                    double[] local = new double[weights[layer][cell].length];
-                    if(layer+1 != outputs.length && multipliers[cell] <= 0) {
-                        Arrays.fill(local,0.0);
-                        continue;
-                    }
+                for (int i = 0; i < local.length; ++i) {
+                    if (layer + 1 != outputs.length) {
+                        double val = multipliers[cell];
 
-                    for(int i=0;i<local.length;++i) {
-                        if(layer+1 != outputs.length) {
-                            double val = multipliers[cell]*inputs[i];
+                        local[i] = val * inputs[i];
+                        nonLGrads[i] += val * weights[layer][cell][i];
+                    } else {
+                        double[] probs = outputs[i];
+                        for (int iPos = 0; iPos < local.length; ++iPos) {
+                            double prob1 = probs[cell];
+                            double prob2 = probs[iPos];
 
-                            local[i] = val;
-                            nonLGrads[i] += val;
-                        } else {
-                            // TODO softmax gradient
+                            double val = (multipliers[cell] * prob1) * ((iPos == cell ? 1 : 0) - prob2);
+
+                            local[i] += val * inputs[i];
+                            nonLGrads[i] += val * weights[layer][cell][iPos];
                         }
                     }
                 }
+            }
 
         }
 
@@ -142,27 +150,26 @@ public class ExpectedLossClassifier {
     public double[][] compute(double[] input) {
         double[][] result = new double[weights.length][];
 
-        for(int i=0;i<weights.length;++i) {
-            double[] ins = i==0 ? input : result[i-1];
+        for (int i = 0; i < weights.length; ++i) {
+            double[] ins = i == 0 ? input : result[i - 1];
             double[][] ws = weights[i];
 
             double[] done = new double[ws.length];
             result[i] = done;
 
             double sum = 0.0;
-            for(int j=0;j<ws.length;++j) {
-                double inner = inner_product(ws[j],ins);
+            for (int j = 0; j < ws.length; ++j) {
+                double inner = inner_product(ws[j], ins);
 
-                sum += done[j] = i == result.length-1 ? Math.exp(inner) : Math.max(0,inner);
+                sum += done[j] = i == result.length - 1 ? Math.exp(inner) : Math.max(0, inner);
             }
 
-            if(i == result.length-1) {
-                for(int j=0;j<done.length;++j) {
+            if (i == result.length - 1) {
+                for (int j = 0; j < done.length; ++j) {
                     done[j] /= sum;
                 }
             }
         }
-
 
         return result;
     }
@@ -175,16 +182,15 @@ public class ExpectedLossClassifier {
     public double[] computeProbabilities(double[] input) {
         double[][] all = this.compute(input);
 
-        return all[all.length-1];
+        return all[all.length - 1];
     }
-
 
     /**
      *
      * @param input
      * @return
      */
-    public int  chooseOne(double[] input) {
+    public int chooseOne(double[] input) {
         return ArrayMath.argmax(this.computeProbabilities(input));
     }
 
@@ -197,8 +203,8 @@ public class ExpectedLossClassifier {
     private double inner_product(double[] w, double[] ins) {
         double amount = 0.0;
 
-        for(int i=0;i<w.length;++i) {
-            amount += w[i]*ins[i];
+        for (int i = 0; i < w.length; ++i) {
+            amount += w[i] * ins[i];
         }
 
         return amount;
---
+++ src/main/java/de/uni_saarland/coli/learning_rates/AdaGrad.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.uni_saarland.coli.learning_rates;
+
+/**
+ *
+ * @author christoph_teichmann
+ */
+public class AdaGrad {
+    
+    
+    
+    
+    
+    
+}
